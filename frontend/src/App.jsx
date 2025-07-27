ffcb6549f879adc30a285b5221c780560f505ce1
Niyaz <niyazsahulhameed@gmail.com>
2025-07-27 17:16:38 +0530

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup"
+import Dashboard from "./pages/Home/Dashboard"
+import InterviewPrep from "./pages/InterviewPrepPage/InterviewPrep"
+import LandingPage from "./pages/LandingPage"
+ 
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/" element={<LandingPage /> } />
+          <Route path="/dashboard" element={<Dashboard /> } />
+          <Route path="/interview-prep/:sessionId" element={<InterviewPrep /> } />
+
+        </Routes>
+      </BrowserRouter>
+
+      <Toaster 
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          }
+        }} />
+    </div>
+  )
+}
---
+++ frontend/src/pages/InterviewPrepPage/InterviewPrep.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function InterviewPrep() {
+  return (
+    <div>InterviewPrep</div>
+  )
+}
